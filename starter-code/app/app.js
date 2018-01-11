627d19c425d8411155674da2bb915f7d475ff29f
Jake Chavez <jake94chavez@gmail.com>
2018-01-11 12:17:09 -0600

    adds user model and Google strategy

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -5,7 +5,7 @@ var bodyParser     = require('body-parser');
 var app            = express();
 var mongoose       = require('mongoose');
 var passport       = require('passport');
-var expressSession = require('express-session');
+var expressSession = require('express.session');
 var cookieParser   = require("cookie-parser");
 const ENV                = require('./app-env');
 const googleClientKey    = ENV.GOOGLE_CLIENT_ID;
@@ -25,10 +25,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.use(app.router);
 
 // Setting up the Passport Strategies
 require("./config/passport")(passport)
+const User = require('../models/user');
+const passport = require('passport');
+const googleClientKey = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
 // Add code here:
 
 app.listen(3000);
