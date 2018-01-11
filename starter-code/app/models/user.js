c1e3c210437b002b162adb031aeadbb8338eab88
Jake Chavez <jake94chavez@gmail.com>
2018-01-11 14:19:18 -0600

    adds findorcreate function and does what its supposed to

--- starter-code/app/models/user.js
+++ starter-code/app/models/user.js
@@ -1,9 +1,14 @@
 var mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate');
+var Schema = mongoose.Schema;
 
-module.exports = mongoose.model('User',{
-  google: {
-    id: String,
-    access_token: String,
-    email: String
+var userSchema = new Schema ({
+	google: {
+    	id: String,
+    	access_token: String,
+    	email: String
   }
 });
+
+userSchema.plugin(findOrCreate);
+module.exports = mongoose.model('User', userSchema);
